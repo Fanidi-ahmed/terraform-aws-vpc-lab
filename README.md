e77cd2c46bcfaafeab3567eee8aed46f04fd7566
Fanidi Ahmed <fanidi.ahmed@example.com>
2026-03-26 09:34:56 +0100

    Terraform AWS VPC lab

---
+++ README.md
@@ -0,0 +1,13 @@
+# 🌐 Terraform AWS VPC Lab
+
+Projet Terraform pour créer une infrastructure réseau AWS :
+
+- VPC
+- Subnet public
+- Internet Gateway
+- Route Table
+
+## Objectif
+
+Comprendre la création d'une architecture réseau AWS avec Terraform.
+
---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "eu-west-3"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR du VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR du subnet public"
+  type        = string
+  default     = "10.0.1.0/24"
+}
